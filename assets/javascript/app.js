ff8bc209397438e07c1f60d9c2e6f76d85b69410
Peter K <peterkohler95@gmail.com>
2018-08-19 13:16:34 -0500

    train schedule app working, still need to fix time format

---
+++ assets/javascript/app.js
@@ -0,0 +1,79 @@
+var database = firebase.database();
+
+console.log(database);
+
+// 2. Button for adding trains
+$("#add-train-btn").on("click", function (event) {
+    event.preventDefault();
+
+    // Grabs user input
+    var trainName = $("#train-name-input").val().trim();
+    var trainDestination = $("#destination-input").val().trim();
+    var trainStartTime = $("#start-input").val().trim();
+    var trainFrequency = $("#frequency-input").val().trim();
+
+    // Creates local "temporary" object for holding train data
+    var newTrain = {
+        name: trainName,
+        destination: trainDestination,
+        start: trainStartTime,
+        frequency: trainFrequency
+    };
+
+    // Uploads employee data to the database
+    database.ref().push(newTrain);
+
+    // Logs everything to console
+    console.log(newTrain);
+
+    // Clears all of the text-boxes
+    $("#train-name-input").val("");
+    $("#destination-input").val("");
+    $("#start-input").val("");
+    $("#frequency-input").val("");
+});
+
+// 3. Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
+database.ref().on("child_added", function (childSnapshot) {
+    console.log(childSnapshot.val());
+
+    // Store everything into a variable.
+    var trainName = childSnapshot.val().name;
+    var trainDestination = childSnapshot.val().destination;
+    var trainStartTime = childSnapshot.val().start;
+    var trainFrequency = childSnapshot.val().frequency;
+
+    // Prettify the train start
+    var trainStartPretty = moment(trainStartTime, "HH:mm");
+    console.log(trainStartPretty);
+
+    // Difference between the times
+    var diffTime = moment().diff(moment(trainStartPretty), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
+
+    // Time apart (remainder)
+    var tRemainder = diffTime % trainFrequency;
+    console.log(tRemainder);
+
+    // Minute Until Train
+    var tMinutesTillTrain = trainFrequency - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+    // Next Train
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+
+
+    // Create the new row
+    var newRow = $("<tr>").append(
+        $("<td>").text(trainName),
+        $("<td>").text(trainDestination),
+        $("<td>").text(trainFrequency),
+        $("<td>").text(nextTrain),
+        $("<td>").text(tMinutesTillTrain),
+    );
+
+    // Append the new row to the table
+    $("#train-table > tbody").append(newRow);
+
+});
